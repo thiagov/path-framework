71828b0236602d43a9a8a919adc8b43b415e4c90
Thiago Vilela <thiagosilvavilela@gmail.com>
2013-07-03 00:49:53 -0300

    Refatorando main

--- main.rb
+++ main.rb
@@ -15,23 +15,18 @@ end_j    = ARGV[3].to_i
 map_name = ARGV[4]
 algorithm = ARGV[5]
 
-# Initialize map
+# Initialize map and observation
 Map.instance.read_map(map_name)
-#Map.instance.print_map
-
-# Initialize observation
 Observation.instance.set_fields
-#Uncomment this line to have full observability
-#Observation.instance.view_all_map
+#Observation.instance.view_all_map #Uncomment this line to have full observability
 
+# Initialize start and goal nodes
 initial_node = Node.new(init_i, init_j)
 goal = Node.new(end_i, end_j)
 
-current_node = initial_node
 
 # Set planner
 planner = nil
-
 case algorithm
 # A* needs full observability to work!
 when "a_star"
@@ -42,11 +37,14 @@ when "lsslrta"
   planner = LssLrta.new
 end
 
+# Create some variables for the planning
 medium_exec_time = 0.0
-longest_time = 0.0
-cnt = 0
-path = []
+longest_time     = 0.0
+cnt              = 0
+path             = []
+current_node     = initial_node
 
+# Insert initial node on path
 path << current_node
 Observation.instance.update_observation(current_node.i, current_node.j)
 
@@ -56,8 +54,11 @@ while !current_node.equals?(goal)
 
   node_candidate = planner.get_move(current_node, goal)
 
-  end_time = Time.now
-  cnt += 1
+  end_time          = Time.now
+  cnt              += 1
+  move_time         = (end_time - start_time)
+  medium_exec_time += move_time
+  longest_time      = move_time if move_time > longest_time
 
   if current_node.is_neighbour?(node_candidate) && Map.instance.is_valid?(node_candidate.i, node_candidate.j) && Map.instance.is_passable?(node_candidate.i, node_candidate.j)
     current_node = node_candidate
@@ -65,10 +66,6 @@ while !current_node.equals?(goal)
     puts "Ocorreu um erro! Posição iniválida!"
     exit
   end
-
-  move_time = (end_time - start_time)
-  medium_exec_time += move_time
-  longest_time = move_time if move_time > longest_time
 
   path << current_node
   Observation.instance.update_observation(current_node.i, current_node.j)
@@ -89,5 +86,6 @@ for i in (0..path.size-2)
   end
 end
 
+# Print output
 #Observation.instance.print_grid(current_node, goal, nil, path)
 puts "#{total_cost} #{medium_exec_time/cnt.to_f} #{longest_time}"
