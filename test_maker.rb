4526a928a02bd2ae40a1993ac892f409262b35e4
Thiago Vilela <thiagosilvavilela@gmail.com>
2013-10-07 20:47:48 -0300

    Ajustanto teste maker

--- test_maker.rb
+++ test_maker.rb
@@ -1,47 +1,63 @@
 #encoding: utf-8
 
 maps = ["arena2.map"]#, "den009d.map", "random512-40-2.map", "maze512-32-0.map"]
+
+conditions = [
+  {:algorithm => "lrta",    :lookaheads => [[1, 0]]},
+  {:algorithm => "prta",    :lookaheads => [[1, 0]]},
+  {:algorithm => "lsslrta", :lookaheads => [[10, 0], [100, 0], [1000, 0]]},
+  {:algorithm => "rtaa",    :lookaheads => [[10, 0], [100, 0], [1000, 0]]},
+  {:algorithm => "tbaa",    :lookaheads => [[10, 0], [100, 0], [1000, 0]]},
+  {:algorithm => "lrta_k",  :lookaheads => [[10, 0], [100, 0], [1000, 0]]},
+  {:algorithm => "plrta",   :lookaheads => [[10, 10], [100, 10], [1000, 10]]},
+  {:algorithm => "tba",     :lookaheads => [[10, 0], [100, 0], [1000, 0]]},
+  {:algorithm => "d_lite",  :lookaheads => [[1, 0]]}
+]
+
 test_algo = ["flatmc"]
 
-test_algo.each do |algorithm|
-  maps.each do |sf|
-    File.open("#{algorithm}_#{sf}", "w") do |outfile|
-      File.open("maps/#{sf}.scen", "r") do |infile|
-        version = infile.gets
+conditions.each do |condition|
+  algorithm  = condition[:algorithm]
+  lookaheads = condition[:lookaheads]
+  lookaheads.each do |lookahead|
+    maps.each do |sf|
+      File.open("results/used/#{algorithm}___#{sf.split(".")[0]}___look-#{lookahead}.csv", "w") do |outfile|
+        File.open("maps/used/#{sf}.scen", "r") do |infile|
+          version = infile.gets
 
-        cnt = 0
-        acc_quality = 0.0
-        acc_med_time = 0.0
-        acc_max_time = 0.0
-        while line = infile.gets
-          line = line.delete("\n")
-          bucket, map, width, height, sx, sy, gx, gy, optimal = line.split(/\t/)
-          sx = sx.to_i
-          sy = sy.to_i
-          gx = gx.to_i
-          gy = gy.to_i
-          optimal = optimal.to_f
+          cnt = 0
+          header = "Teste,Bucket,Estados expandidos,Episódios de busca,Custo da trajetória,% Sol. Ótima,Ações por Episódio de Busca,Tempo Total Busca,Tempo Médio Episódio de Busca,Tempo Médio Busca por Ação"
+          outfile.puts(header)
+          while line = infile.gets
+            line = line.delete("\n")
+            bucket, map, width, height, sx, sy, gx, gy, optimal = line.split(/\t/)
+            sx = sx.to_i
+            sy = sy.to_i
+            gx = gx.to_i
+            gy = gy.to_i
+            optimal = optimal.to_f
 
-          result = %x[ruby main.rb #{sy} #{sx} #{gy} #{gx} #{sf} #{algorithm}]
-          path_size, med_time, max_time = result.split(" ")
+            result = %x[ruby main.rb #{sy} #{sx} #{gy} #{gx} #{sf} #{algorithm} #{lookahead} #{queue_size}]
+            expanded, episodes, cost, action_per_episode, total_time, episode_time, action_time = result.split("\n")
 
-          path_size = path_size.to_f
-          path_size = path_size - optimal
+            expanded           = expanded.split(": ")[1].to_f
+            episodes           = episodes.split(": ")[1].to_i
+            cost               = cost.split(": ")[1].to_f
+            action_per_episode = action_per_episode.split(": ")[1].to_f
+            total_time         = total_time.split(": ")[1].to_f
+            episode_time       = episode_time.split(": ")[1].to_f
+            action_time        = action_time.split(": ")[1].to_f
 
+            cost_quality = (cost - optimal)/optimal
+            cost_quality = 0.0 if cost_quality < 0.0
 
-          med_time = med_time.to_f
-          max_time = max_time.to_f
+            path_size    = path_size.to_f
+            path_quality = (path_size - optimal)/optimal
 
-          acc_quality += path_size
-          acc_med_time += med_time
-          acc_max_time += max_time
-          cnt += 1
-
-          outfile.puts(result)
+            cnt += 1
+            outfile.puts("#{cnt},#{bucket},#{expanded},#{episodes},#{cost},#{cost_quality},#{action_per_episode},#{total_time},#{episode_time},#{action_time}")
+          end
         end
-        outfile.puts("Subotimalidade: #{acc_quality/cnt}")
-        outfile.puts("Tempo médio médio: #{acc_med_time/cnt}")
-        outfile.puts("Tempo máximo médio: #{acc_max_time/cnt}")
       end
     end
   end
