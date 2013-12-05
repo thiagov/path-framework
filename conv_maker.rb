5c9cdc2cff0a7d398dcffa616cf9f36ffe9b1de5
Thiago Vilela <thiagosilvavilela@gmail.com>
2013-12-05 13:26:05 -0200

    Adicionando testes de convergencia

---
+++ conv_maker.rb
@@ -0,0 +1,68 @@
+#encoding: utf-8
+require "pp"
+
+#TODO: Teste de convergencia. Deve ser feito para:
+# d_lite  - Não aplicável?
+# lrta    - OK
+# prta    - Não aplicável
+# lsslrta - OK
+# rtaa    - OK
+# tbaa    - OK
+# lrta_k  - OK
+# plrta   - OK
+# tba     - Não aplicável?
+
+maps = ["arena.map", "den101d.map"]
+
+conditions = [
+#  {:algorithm => "d_lite",  :lookaheads => [[1, 0]]}, #OK!
+  {:algorithm => "lrta",    :lookaheads => [[1, 0]]}, #OK!
+#  {:algorithm => "prta",    :lookaheads => [[1, 0]]}, #OK!
+  {:algorithm => "lsslrta", :lookaheads => [[90, 0], [150, 0], [230, 0], [310, 0]]}, #OK!
+  {:algorithm => "rtaa",    :lookaheads => [[180, 0], [450, 0], [800, 0], [1100, 0]]}, #OK
+  {:algorithm => "tbaa",    :lookaheads => [[180, 0], [420, 0], [660, 0], [880, 0]]}, #OK!
+  {:algorithm => "lrta_k",  :lookaheads => [[10000, 0]]}, #OK!
+  {:algorithm => "plrta",   :lookaheads => [[140, 100], [270, 100], [480, 100], [700, 100]]} #OK!
+#  {:algorithm => "tba",     :lookaheads => [[230, 0], [450, 0], [800, 0], [1100, 0]} #OK!
+]
+
+conditions.each do |condition|
+  algorithm  = condition[:algorithm]
+  lookaheads = condition[:lookaheads]
+  lookaheads.each do |lookahead_tuple|
+    lookahead  = lookahead_tuple[0]
+    queue_size = lookahead_tuple[1]
+    maps.each do |sf|
+      File.open("results/used/#{algorithm}___#{sf.split(".")[0]}___look-#{lookahead}.csv", "w") do |outfile|
+        File.open("maps/used/#{sf}.scen", "r") do |infile|
+          version = infile.gets
+
+          cnt = 0
+          header = "Teste,Bucket,Num. Solves,Total Time,Final Cost"
+          outfile.puts(header)
+          while line = infile.gets
+            line = line.delete("\n")
+            bucket, map, width, height, sx, sy, gx, gy, optimal = line.split(/\s/)
+            sx = sx.to_i
+            sy = sy.to_i
+            gx = gx.to_i
+            gy = gy.to_i
+            optimal = optimal.to_f
+
+            result = %x[ruby convergence.rb #{sy} #{sx} #{gy} #{gx} #{sf} #{algorithm} #{lookahead} #{queue_size}]
+            total_solves, total_time, final_cost = result.split("\n")
+
+            total_solves = total_solves.split(": ")[1].to_f
+            total_time   = total_time.split(": ")[1].to_f
+            final_cost   = final_cost.split(": ")[1].to_f
+
+
+            cnt += 1
+            outfile.puts("#{cnt},#{bucket},#{total_solves},#{total_time},#{final_cost}")
+          end
+        end
+      end
+      pp "Finalizado mapa #{sf} para #{algorithm} com lookaheads #{lookahead_tuple}"
+    end
+  end
+end
