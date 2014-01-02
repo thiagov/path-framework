052e2e87d759a71108b81baad2eb33ac61294f41
Matheus Silva Vilela <matheusvilela@gmail.com>
2014-01-02 11:39:57 -0200

    Mandando email com o test)maker

--- test_maker.rb
+++ test_maker.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
 require "pp"
+require "pony"
 
 #TODO: Teste de convergencia. Deve ser feito para:
 # d_lite  - Não aplicável?
@@ -12,20 +13,21 @@ require "pp"
 # plrta   - OK
 # tba     - Não aplicável?
 
-maps = ["maze512-16-2.map", "random512-35-3.map"]#"combat.map", "lak503d.map", "arena2.map", "duskwood.map"]
+maps = ["random512-35-3.map", "maze512-16-2.map"]#"combat.map", "lak503d.map", "arena2.map", "duskwood.map"]
 
 conditions = [
-  {:algorithm => "d_lite",  :lookaheads => [[1, 0]]}, #OK!
-  {:algorithm => "lrta",    :lookaheads => [[1, 0]]}, #OK!
-  {:algorithm => "prta",    :lookaheads => [[1, 0]]}, #OK!
-  {:algorithm => "lsslrta", :lookaheads => [[90, 0], [150, 0], [230, 0], [310, 0]]}, #OK!
-  {:algorithm => "rtaa",    :lookaheads => [[180, 0], [450, 0], [800, 0], [1100, 0]]}, #OK
+#  {:algorithm => "d_lite",  :lookaheads => [[1, 0]]}, #OK!
+#  {:algorithm => "lrta",    :lookaheads => [[1, 0]]}, #OK!
+#  {:algorithm => "prta",    :lookaheads => [[1, 0]]}, #OK!
+#  {:algorithm => "lsslrta", :lookaheads => [[90, 0], [150, 0], [230, 0], [310, 0]]}, #OK!
+#  {:algorithm => "rtaa",    :lookaheads => [[180, 0], [450, 0], [800, 0], [1100, 0]]}, #OK
+#  {:algorithm => "lrta_k",  :lookaheads => [[10000, 0]]}, #OK!
   {:algorithm => "tbaa",    :lookaheads => [[180, 0], [420, 0], [660, 0], [880, 0]]}, #OK!
-  {:algorithm => "lrta_k",  :lookaheads => [[10000, 0]]}, #OK!
   {:algorithm => "plrta",   :lookaheads => [[140, 100], [270, 100], [480, 100], [700, 100]]} #OK!
 #  {:algorithm => "tba",     :lookaheads => [[230, 0], [450, 0], [800, 0], [1100, 0]} #OK!
 ]
 
+begin
 conditions.each do |condition|
   algorithm  = condition[:algorithm]
   lookaheads = condition[:lookaheads]
@@ -79,4 +81,47 @@ conditions.each do |condition|
       pp "Finalizado mapa #{sf} para #{algorithm} com lookaheads #{lookahead_tuple}"
     end
   end
+end
+
+Pony.mail({
+  :to => 'thiagosilvavilela@gmail.com',
+  :subject => 'Teste terminado',
+  :body => 'Acabaram os testes :)',
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'thiago@superficie.org',
+    :password             => 'sulamerica',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+})
+
+
+rescue => error
+
+Pony.mail({
+  :to => 'thiagosilvavilela@gmail.com',
+  :subject => 'Erro no script',
+  :body => 'Ocorreu um erro :(',
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'thiago@superficie.org',
+    :password             => 'sulamerica',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+})
+
+puts "="*20
+puts error
+puts "="*20
+puts error.backtrace
+puts "="*20
+
 end
